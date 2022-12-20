1bf88a322d968c077ae16ad166d4100cf68dbb49
goncrust <goncalonrua@protonmail.com>
2022-12-20 20:31:08 +0000

    add equal_files funcs to util.h

--- tests/extra_symlinks_test.c
+++ tests/extra_symlinks_test.c
@@ -1,4 +1,5 @@
 #include "operations.h"
+#include "util.h"
 
 #include <assert.h>
 #include <pthread.h>
@@ -11,36 +12,7 @@
 const char *source_paths[] = {"tests/empty_file.txt", "tests/file_to_copy.txt",
                               "tests/file_to_copy_over512.txt"};
 const char *dest_paths[] = {"/f1", "/f2", "/f3"};
-
 const char *symlink_paths[] = {"/f1_l", "/f2_l", "/f3_l"};
-
-#define BLOCK_SIZE 1024
-
-int equal_files(int tfs_1, int tfs_2) {
-    char tfs_1_buffer[BLOCK_SIZE], tfs_2_buffer[BLOCK_SIZE];
-    ssize_t tfs_1_result ,tfs_2_result;
-
-    memset(tfs_1_buffer, 0, BLOCK_SIZE);
-    memset(tfs_2_buffer, 0, BLOCK_SIZE);
-    while ((tfs_1_result = tfs_read(tfs_1, tfs_1_buffer, BLOCK_SIZE)) != 0 && (tfs_2_result = tfs_read(tfs_2, tfs_2_buffer, BLOCK_SIZE)) != 0) {
-
-        if (tfs_1_result == -1 || tfs_2_result == -1)
-            return -1;
-
-        if (tfs_1_result != tfs_2_result )
-            return -1;
-
-        if (strncmp(tfs_1_buffer, tfs_2_buffer, BLOCK_SIZE) != 0)
-            return -1;
-    }
-
-    tfs_1_result = tfs_read(tfs_1, tfs_1_buffer, BLOCK_SIZE);
-    tfs_2_result = tfs_read(tfs_2, tfs_2_buffer, BLOCK_SIZE);
-    if (tfs_1_result != 0 || tfs_2_result != 0)
-        return -1;
-
-    return 0;
-}
 
 int main(void) {
 
--- tests/threaded_link_2.c
+++ tests/threaded_link_2.c
@@ -1,4 +1,5 @@
 #include "operations.h"
+#include "util.h"
 
 #include <assert.h>
 #include <pthread.h>
@@ -10,34 +11,6 @@
 
 const char *source_path = "tests/file_to_copy.txt";
 const char *dest_path = "/f1";
-
-#define BLOCK_SIZE 1024
-
-int equal_files(int tfs_1, int tfs_2) {
-    char tfs_1_buffer[BLOCK_SIZE], tfs_2_buffer[BLOCK_SIZE];
-    ssize_t tfs_1_result ,tfs_2_result;
-
-    memset(tfs_1_buffer, 0, BLOCK_SIZE);
-    memset(tfs_2_buffer, 0, BLOCK_SIZE);
-    while ((tfs_1_result = tfs_read(tfs_1, tfs_1_buffer, BLOCK_SIZE)) != 0 && (tfs_2_result = tfs_read(tfs_2, tfs_2_buffer, BLOCK_SIZE)) != 0) {
-
-        if (tfs_1_result == -1 || tfs_2_result == -1)
-            return -1;
-
-        if (tfs_1_result != tfs_2_result )
-            return -1;
-
-        if (strncmp(tfs_1_buffer, tfs_2_buffer, BLOCK_SIZE) != 0)
-            return -1;
-    }
-
-    tfs_1_result = tfs_read(tfs_1, tfs_1_buffer, BLOCK_SIZE);
-    tfs_2_result = tfs_read(tfs_2, tfs_2_buffer, BLOCK_SIZE);
-    if (tfs_1_result != 0 || tfs_2_result != 0)
-        return -1;
-
-    return 0;
-}
 
 void *link_file(void *id) {
     int i = *((int *)id);
