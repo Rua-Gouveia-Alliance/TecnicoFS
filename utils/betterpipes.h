c79f66f7b369173c2659655955fe332a2bc406ee
Joao Gouveia <jjtucki4@gmail.com>
2022-12-27 17:34:25 +0000

    Add unlink

--- utils/betterpipes.h
+++ utils/betterpipes.h
@@ -5,6 +5,7 @@
 
 #define MK_FIFO(path)                                                          \
     {                                                                          \
+        unlink(path);                                                          \
         int m_err = mkfifo(path, 0777);                                        \
         ALWAYS_ASSERT(m_err != -1 || errno == EEXIST, "mkfifo critical error") \
     }
