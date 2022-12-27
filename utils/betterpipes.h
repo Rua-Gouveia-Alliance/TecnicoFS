7355e8d2af2a2175ee4d32745257b2c162d94443
Joao Gouveia <jjtucki4@gmail.com>
2022-12-27 15:36:09 +0000

    Create betterpipes.h

---
+++ utils/betterpipes.h
@@ -0,0 +1,12 @@
+#ifndef _BETTER_PIPES_H
+#define _BETTER_PIPES_H
+
+#include "betterassert.h"
+
+#define MK_FIFO(path)                                                          \
+    {                                                                          \
+        int m_err = mkfifo(path, 0777);                                        \
+        ALWAYS_ASSERT(m_err != -1 || errno == EEXIST, "mkfifo critical error") \
+    }
+
+#endif
