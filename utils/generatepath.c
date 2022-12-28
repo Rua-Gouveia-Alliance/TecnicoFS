bac5514dcac9a174c8aa2d7e660fc7cf52cb6963
Joao Gouveia <jjtucki4@gmail.com>
2022-12-28 15:21:07 +0000

    Create generate path

---
+++ utils/generatepath.c
@@ -0,0 +1,12 @@
+#include "generatepath.h"
+#include "serverrequests.h"
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void generate_path(char *dest) {
+    // Using PID to guarantee that the name is unique
+    pid_t pid = getpid();
+    snprintf(dest, PIPE_SIZE, "%d.pipe", pid);
+}
---
+++ utils/generatepath.h
@@ -0,0 +1,6 @@
+#ifndef _GENERATE_PATH_H
+#define _GENERATE_PATH_H
+
+void generate_path(char* dest);
+
+#endif
