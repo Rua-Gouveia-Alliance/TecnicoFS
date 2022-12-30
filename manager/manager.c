41169d8d839a235da058e479102f076173f246c6
goncrust <goncalonrua@gmail.com>
2022-12-30 17:05:04 +0000

    manager (not finished)

--- manager/manager.c
+++ manager/manager.c
@@ -1,7 +1,15 @@
+#include "betterpipes.h"
+#include "clients/opcodes.h"
+#include "generatepath.h"
 #include "logging.h"
+#include "serverrequests.h"
 #include "string.h"
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 static void print_usage() {
     fprintf(stderr, "usage: \n"
@@ -16,14 +24,26 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
-    if (argc == 4 && !strcmp(argv[2], "create")) {
-        // create
-    } else if (argc == 4 && !strcmp(argv[2], "remove")) {
-        // remove
-    } else if (argc == 3 && !strcmp(argv[2], "list")) {
-        // list
-    } else {
+    // Creating the FIFO
+    char path[PIPE_SIZE];
+    generate_path(path);
+    MK_FIFO(path);
+
+    char request[REQUEST_SIZE];
+    if (argc == 4 && !strcmp(argv[2], "create"))
+        create_request(request, BOX_CREATION, path, argv[3]);
+    else if (argc == 4 && !strcmp(argv[2], "remove"))
+        create_request(request, BOX_DELETION, path, argv[3]);
+    else if (argc == 3 && !strcmp(argv[2], "list"))
+        create_list_request(request, path);
+    else {
         print_usage();
         exit(EXIT_FAILURE);
     }
+
+    // Opening the server FIFO and sending the request
+    int register_fd = open(argv[1], O_WRONLY);
+    ALWAYS_ASSERT(register_fd != -1, "invalid register_pipe");
+    write(register_fd, request, REQUEST_SIZE);
+    close(register_fd);
 }
