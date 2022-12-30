bfe93f47dad1c961b24f530dc746ef8e03654165
goncrust <goncalonrua@gmail.com>
2022-12-30 17:02:17 +0000

    serverrequests for manager

--- utils/serverrequests.c
+++ utils/serverrequests.c
@@ -1,15 +1,16 @@
 #include "serverrequests.h"
 #include "betterassert.h"
+#include "clients/opcodes.h"
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
 
 void create_request(char *dest, int op_code, char *session_pipe, char *box) {
 
-    char pipe_name[PIPE_SIZE], box_name[BOX_SIZE], op_code_str[OP_CODE_SIZE];
+    char pipe_name[PIPE_SIZE], box_name[BOX_SIZE], op_code_str[INT_SIZE];
 
-    snprintf(op_code_str, OP_CODE_SIZE, "%d", op_code);
-    op_code_str[OP_CODE_SIZE - 1] = '\0';
+    snprintf(op_code_str, INT_SIZE, "%d", op_code);
+    op_code_str[INT_SIZE - 1] = '\0';
 
     strncpy(pipe_name, session_pipe, PIPE_SIZE - 1);
     pipe_name[PIPE_SIZE - 1] = '\0';
@@ -17,22 +18,86 @@ void create_request(char *dest, int op_code, char *session_pipe, char *box) {
     strncpy(box_name, box, BOX_SIZE - 1);
     box_name[BOX_SIZE - 1] = '\0';
 
-    memcpy(dest, op_code_str, OP_CODE_SIZE);
-    memcpy(dest + OP_CODE_SIZE, pipe_name, PIPE_SIZE);
-    memcpy(dest + OP_CODE_SIZE + PIPE_SIZE, box_name, BOX_SIZE);
+    memcpy(dest, op_code_str, INT_SIZE);
+    memcpy(dest + INT_SIZE, pipe_name, PIPE_SIZE);
+    memcpy(dest + INT_SIZE + PIPE_SIZE, box_name, BOX_SIZE);
+}
+
+void create_list_request(char *dest, char *session_pipe) {
+    char pipe_name[PIPE_SIZE], box_name[BOX_SIZE], op_code_str[INT_SIZE];
+
+    snprintf(op_code_str, INT_SIZE, "%d", BOX_LIST);
+    op_code_str[INT_SIZE - 1] = '\0';
+
+    strncpy(pipe_name, session_pipe, PIPE_SIZE - 1);
+    pipe_name[PIPE_SIZE - 1] = '\0';
+
+    memcpy(dest, op_code_str, INT_SIZE);
+    memcpy(dest + INT_SIZE, pipe_name, PIPE_SIZE);
+    memset(dest + INT_SIZE + PIPE_SIZE, '\0', BOX_SIZE);
+}
+
+void create_response(char *dest, int op_code, int return_code, char *error) {
+    char error_msg[ERROR_SIZE], return_code_str[INT_SIZE],
+        op_code_str[INT_SIZE];
+
+    snprintf(op_code_str, INT_SIZE, "%d", op_code);
+    op_code_str[INT_SIZE - 1] = '\0';
+
+    snprintf(return_code_str, INT_SIZE, "%d", return_code);
+    op_code_str[INT_SIZE - 1] = '\0';
+
+    strncpy(error_msg, error, ERROR_SIZE - 1);
+    error_msg[ERROR_SIZE - 1] = '\0';
+
+    memcpy(dest, op_code_str, INT_SIZE);
+    memcpy(dest + INT_SIZE, return_code_str, INT_SIZE);
+    memcpy(dest + 2 * INT_SIZE, error_msg, ERROR_SIZE);
+}
+
+void create_list_response(char *dest, int last, char *box, int box_size,
+                          int n_publishers, int n_subscribers) {
+    char last_str[INT_SIZE], box_name[BOX_SIZE], box_size_str[INT_SIZE],
+        n_publishers_str[INT_SIZE], n_subscribers_str[INT_SIZE],
+        op_code_str[INT_SIZE];
+
+    snprintf(op_code_str, INT_SIZE, "%d", BOX_LIST_ANS);
+    op_code_str[INT_SIZE - 1] = '\0';
+
+    snprintf(last_str, INT_SIZE, "%d", last);
+    last_str[INT_SIZE - 1] = '\0';
+
+    strncpy(box_name, box, BOX_SIZE - 1);
+    box_name[BOX_SIZE - 1] = '\0';
+
+    snprintf(box_size_str, INT_SIZE, "%d", box_size);
+    box_size_str[INT_SIZE - 1] = '\0';
+
+    snprintf(n_publishers_str, INT_SIZE, "%d", n_publishers);
+    n_publishers_str[INT_SIZE - 1] = '\0';
+
+    snprintf(n_subscribers_str, INT_SIZE, "%d", n_subscribers);
+    n_subscribers_str[INT_SIZE - 1] = '\0';
+
+    memcpy(dest, op_code_str, INT_SIZE);
+    memcpy(dest + INT_SIZE, last_str, INT_SIZE);
+    memcpy(dest + 2 * INT_SIZE, box_name, BOX_SIZE);
+    memcpy(dest + 2 * INT_SIZE + BOX_SIZE, box_size_str, INT_SIZE);
+    memcpy(dest + 3 * INT_SIZE + BOX_SIZE, n_publishers_str, INT_SIZE);
+    memcpy(dest + 4 * INT_SIZE + BOX_SIZE, n_subscribers_str, INT_SIZE);
 }
 
 void create_message(char *dest, int op_code, char *message) {
-    char contents[CONTENTS_SIZE], op_code_str[OP_CODE_SIZE];
+    char contents[CONTENTS_SIZE], op_code_str[INT_SIZE];
 
-    snprintf(op_code_str, OP_CODE_SIZE, "%d", op_code);
-    op_code_str[OP_CODE_SIZE - 1] = '\0';
+    snprintf(op_code_str, INT_SIZE, "%d", op_code);
+    op_code_str[INT_SIZE - 1] = '\0';
 
     strncpy(contents, message, CONTENTS_SIZE - 1);
     contents[CONTENTS_SIZE - 1] = '\0';
-    
-    memcpy(dest, op_code_str, OP_CODE_SIZE);
-    memcpy(dest + OP_CODE_SIZE, contents, CONTENTS_SIZE);
+
+    memcpy(dest, op_code_str, INT_SIZE);
+    memcpy(dest + INT_SIZE, contents, CONTENTS_SIZE);
 }
 
 void parse_request(char *request, int *op_code, char *session_pipe,
@@ -42,13 +107,61 @@ void parse_request(char *request, int *op_code, char *session_pipe,
     *op_code = (int)strtol(request, &endptr, 10);
     ALWAYS_ASSERT(errno == 0 && endptr != request, "invalid op_code\n");
 
-    strncpy(session_pipe, request + OP_CODE_SIZE, PIPE_SIZE - 1);
+    strncpy(session_pipe, request + INT_SIZE, PIPE_SIZE - 1);
     session_pipe[PIPE_SIZE - 1] = '\0';
 
-    strncpy(box_name, request + OP_CODE_SIZE + PIPE_SIZE, BOX_SIZE - 1);
+    strncpy(box_name, request + INT_SIZE + PIPE_SIZE, BOX_SIZE - 1);
     box_name[BOX_SIZE - 1] = '\0';
 
     snprintf(box_path, BOX_PATH_SIZE, "/%s", box_name);
+}
+
+void parse_response(char *response, int *op_code, int *return_code,
+                    char *error) {
+    errno = 0;
+    char *endptr;
+
+    *op_code = (int)strtol(response, &endptr, 10);
+    ALWAYS_ASSERT(errno == 0 && endptr != response, "invalid op_code\n");
+
+    *return_code = (int)strtol(response + INT_SIZE, &endptr, 10);
+    ALWAYS_ASSERT(errno == 0 && endptr != response + INT_SIZE,
+                  "invalid op_code\n");
+
+    strncpy(error, response + 2 * INT_SIZE, ERROR_SIZE - 1);
+    error[ERROR_SIZE - 1] = '\0';
+}
+
+void parse_list_response(char *response, int *op_code, int *last,
+                         char *box_path, int *box_size, int *n_publishers,
+                         int *n_subscribers) {
+    errno = 0;
+    char *endptr, box_name[BOX_SIZE];
+
+    *op_code = (int)strtol(response, &endptr, 10);
+    ALWAYS_ASSERT(errno == 0 && endptr != response, "invalid op_code\n");
+
+    *last = (int)strtol(response + INT_SIZE, &endptr, 10);
+    ALWAYS_ASSERT(errno == 0 && endptr != response + INT_SIZE,
+                  "invalid op_code\n");
+
+    strncpy(box_name, response + 2 * INT_SIZE, BOX_SIZE - 1);
+    box_name[BOX_SIZE - 1] = '\0';
+    snprintf(box_path, BOX_PATH_SIZE, "/%s", box_name);
+
+    *box_size = (int)strtol(response + 2 * INT_SIZE + BOX_SIZE, &endptr, 10);
+    ALWAYS_ASSERT(errno == 0 && endptr != response + 2 * INT_SIZE + BOX_SIZE,
+                  "invalid op_code\n");
+
+    *n_publishers =
+        (int)strtol(response + 3 * INT_SIZE + BOX_SIZE, &endptr, 10);
+    ALWAYS_ASSERT(errno == 0 && endptr != response + 3 * INT_SIZE + BOX_SIZE,
+                  "invalid op_code\n");
+
+    *n_subscribers =
+        (int)strtol(response + 4 * INT_SIZE + BOX_SIZE, &endptr, 10);
+    ALWAYS_ASSERT(errno == 0 && endptr != response + 4 * INT_SIZE + BOX_SIZE,
+                  "invalid op_code\n");
 }
 
 void parse_message(char *message, int *op_code, char *contents,
@@ -58,7 +171,7 @@ void parse_message(char *message, int *op_code, char *contents,
     *op_code = (int)strtol(message, &endptr, 10);
     ALWAYS_ASSERT(errno == 0 && endptr != message, "invalid op_code\n");
 
-    strncpy(contents, message + OP_CODE_SIZE, CONTENTS_SIZE - 1);
+    strncpy(contents, message + INT_SIZE, CONTENTS_SIZE - 1);
     contents[CONTENTS_SIZE - 1] = '\0';
 
     // Removing newline character
--- utils/serverrequests.h
+++ utils/serverrequests.h
@@ -3,24 +3,35 @@
 
 #define CONTENTS_SIZE 1024
 #define PIPE_SIZE 256
+#define ERROR_SIZE 1024
 #define BOX_SIZE 32
 #define BOX_PATH_SIZE 33
-#define OP_CODE_SIZE 3
-#define REQUEST_SIZE OP_CODE_SIZE + PIPE_SIZE + BOX_SIZE
-#define MESSAGE_SIZE OP_CODE_SIZE + CONTENTS_SIZE
+#define INT_SIZE 3
+#define REQUEST_SIZE INT_SIZE + PIPE_SIZE + BOX_SIZE
+#define MESSAGE_SIZE INT_SIZE + CONTENTS_SIZE
+#define RESPONSE_SIZE 2 * INT_SIZE + ERROR_SIZE
+#define LIST_RESPONSE_SIZE 5 * INT_SIZE + BOX_SIZE
 
 #include <stdlib.h>
 
 void create_request(char *dest, int op_code, char *session_pipe, char *box);
-void create_list_request();
+void create_list_request(char *dest, char *session_pipe);
 
-void create_return();
-void create_list_return();
+void create_response(char *dest, int op_code, int return_code, char *error);
+void create_list_response(char *dest, int last, char *box, int box_size,
+                          int n_publishers, int n_subscribers);
 
 void create_message(char *dest, int op_code, char *message);
 
 void parse_request(char *request, int *op_code, char *session_pipe,
                    char *box_path);
+
+void parse_response(char *response, int *op_code, int *return_code,
+                    char *error);
+
+void parse_list_response(char *response, int *op_code, int *last,
+                         char *box_path, int *box_size, int *n_publishers,
+                         int *n_subscribers);
 
 void parse_message(char *message, int *op_code, char *contents,
                    size_t *contents_size);
